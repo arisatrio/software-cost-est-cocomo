a8bcc28979e27512dd3813961927e02ada94272d
arisatrio <arysatrio097@gmail.com>
2025-09-20 20:14:19 +0700

    seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Ari Satrio',
+            'email' => 'admin@main.com',
+            'password' => bcrypt('password'), // Use a secure password in production
         ]);
     }
 }
